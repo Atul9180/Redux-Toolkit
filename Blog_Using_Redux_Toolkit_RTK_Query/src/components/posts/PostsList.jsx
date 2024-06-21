a3b4a01a7101d88397781b3f3ab3f351404d6e63
Atul <patelatulpatel@gmail.com>
2024-06-21 16:10:58 +0530

    order posts w.r.s.t time of post

--- Blog_Using_Redux_Toolkit_RTK_Query/src/components/posts/PostsList.jsx
+++ Blog_Using_Redux_Toolkit_RTK_Query/src/components/posts/PostsList.jsx
@@ -1,5 +1,4 @@
 import { useSelector } from "react-redux";
-// import { decrement } from "../../features/posts/postsSlice"
 import { selectAllPosts } from "../../features/posts/postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
@@ -7,7 +6,9 @@ import TimeAgo from "./TimeAgo";
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderPosts = posts.map((post) => (
+  const orderedPosts = posts.slice((a, b) => b.date.localCompare(a.date));
+
+  const renderPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
